db9641a76e3862a50795d2bfa5b27e3dcb5c7ac3
Ram Iyer <ramky.iyer@gmail.com>
2022-02-05 22:38:43 -0500

    adding state to store searchString using useState hook

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,23 @@
+import {useState} from 'react'
+
 import "bootstrap/dist/css/bootstrap.min.css"
 
 import Header from "./components/Header"
 import Search from "./components/Search"
 
+
 function App() {
+  const [searchString, setSearchString] = useState('');
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(searchString);
+  };
+
   return (
     <div>
       <Header title="Images Gallery" />
-      <Search />
+      <Search searchString={searchString} setSearchString={setSearchString} handleSubmit={handleSearchSubmit} />
     </div>
   );
 }
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import { Container, Row, Col, Form, Button } from 'react-bootstrap'
 
-const Search = () => {
+const Search = ({ handleSubmit, searchString, setSearchString }) => {
   return (
     <Container className="mt-4">
       <Row className="justify-content-center">
         <Col xs={12} md={8}>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Row>
               <Col xs={9}>
-                <Form.Control type="text" placeholder="Search for new image ..."></Form.Control>
+                <Form.Control
+                  type="text"
+                  placeholder="Search for new image ..."
+                  value={searchString}
+                  onChange={(e) => setSearchString(e.target.value)} >
+                </Form.Control>
               </Col>
               <Col>
                 <Button variant="primary" type="submit">Search</Button>
