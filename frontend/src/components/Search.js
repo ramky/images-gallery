3919c6bb61d95d6820ac54e074be4bc43055104c
Ram Iyer <ramky.iyer@gmail.com>
2022-02-05 21:48:44 -0500

    styling Header and Search components

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -3,12 +3,12 @@ import { Container, Row, Col, Form, Button } from 'react-bootstrap'
 
 const Search = () => {
   return (
-    <Container>
-      <Row>
-        <Col>
+    <Container className="mt-4">
+      <Row className="justify-content-center">
+        <Col xs={12} md={8}>
           <Form>
             <Row>
-              <Col>
+              <Col xs={9}>
                 <Form.Control type="text" placeholder="Search for new image ..."></Form.Control>
               </Col>
               <Col>
