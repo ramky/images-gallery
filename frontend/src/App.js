64566f1bda8da6324be853a93dafe54c13d98c3b
Ram Iyer <ramky.iyer@gmail.com>
2022-02-05 21:25:29 -0500

    added search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css"
 
-import Header from "./components/Header";
+import Header from "./components/Header"
+import Search from "./components/Search"
 
 function App() {
   return (
     <div>
       <Header title="Images Gallery" />
+      <Search />
     </div>
   );
 }
---
+++ frontend/src/components/Search.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Container, Row, Col, Form, Button } from 'react-bootstrap'
+
+const Search = () => {
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <Form>
+            <Row>
+              <Col>
+                <Form.Control type="text" placeholder="Search for new image ..."></Form.Control>
+              </Col>
+              <Col>
+                <Button variant="primary" type="submit">Search</Button>
+              </Col>
+            </Row>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default Search
