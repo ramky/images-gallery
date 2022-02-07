0c98340bb93bac6215bc6c985d07541d40f4a71b
Ram Iyer <riyer@teladochealth.com>
2022-02-06 22:34:24 -0500

    making unsplash api via fetch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,16 +5,23 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import Header from "./components/Header"
 import Search from "./components/Search"
 
+const UNSPLASH_ACCESS_KEY = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
+const UNSPLASH_API_URL = 'https://api.unsplash.com';
 
 function App() {
   const [searchString, setSearchString] = useState('');
 
-  const UNSPLASH_ACCESS_KEY = process.key.REACT_APP_UNSPLASH_ACCESS_KEY;
-  const UNSPLASH_SECRET_KEY = process.key.REACT_APP_UNSPLASH_SECRET_KEY;
-
   const handleSearchSubmit = (e) => {
     e.preventDefault();
-    console.log(searchString);
+    console.log(`Searching for ${searchString} via UNSPLASH API`);
+    fetch(`${UNSPLASH_API_URL}/photos/random?query=${searchString}&client_id=${UNSPLASH_ACCESS_KEY}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(err => {
+        console.log(`Following error occured: ${err}`);
+      });
   };
 
   return (
