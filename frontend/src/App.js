170558f61c20fac99c6f4a30b0739dadfd0ea859
Ram Iyer <riyer@teladochealth.com>
2022-02-07 16:55:23 -0500

    prettier and eslint changes for project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
-import {useState} from 'react'
+import { useState } from 'react';
 
-import "bootstrap/dist/css/bootstrap.min.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Header from "./components/Header"
-import Search from "./components/Search"
+import Header from './components/Header';
+import Search from './components/Search';
 
 const UNSPLASH_ACCESS_KEY = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
 const UNSPLASH_API_URL = 'https://api.unsplash.com';
@@ -14,12 +14,14 @@ function App() {
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(`Searching for ${searchString} via UNSPLASH API`);
-    fetch(`${UNSPLASH_API_URL}/photos/random?query=${searchString}&client_id=${UNSPLASH_ACCESS_KEY}`)
-      .then(res => res.json())
-      .then(data => {
+    fetch(
+      `${UNSPLASH_API_URL}/photos/random?query=${searchString}&client_id=${UNSPLASH_ACCESS_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
         console.log(data);
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(`Following error occured: ${err}`);
       });
     setSearchString('');
@@ -28,7 +30,11 @@ function App() {
   return (
     <div>
       <Header title="Images Gallery" />
-      <Search searchString={searchString} setSearchString={setSearchString} handleSubmit={handleSearchSubmit} />
+      <Search
+        searchString={searchString}
+        setSearchString={setSearchString}
+        handleSubmit={handleSearchSubmit}
+      />
     </div>
   );
 }
