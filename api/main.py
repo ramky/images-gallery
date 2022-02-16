dd53d227122112155c3f0f60d7c04e437b5e9913
Ram Iyer <riyer@teladochealth.com>
2022-02-16 16:06:48 -0500

    adding requests, os and making unsplash api call

--- api/main.py
+++ api/main.py
@@ -1,4 +1,9 @@
 from flask import Flask, request
+import os
+import requests
+
+UNSPASH_URL = "https://api.unsplash.com/photos/random"
+UNSPLASH_KEY = os.getenv("REACT_APP_UNSPLASH_ACCESS_KEY")
 
 app = Flask(__name__)
 
@@ -6,7 +11,12 @@ app = Flask(__name__)
 @app.route("/new-image")
 def new_image():
     search_term = request.args.get("query")
-    return {"search_term": search_term}
+    headers = {"Authorization": "Client-ID " + UNSPLASH_KEY, "Accept-Version": "v1"}
+    print(headers)
+    params = {"query": search_term}
+    response = requests.get(url=UNSPASH_URL, headers=headers, params=params)
+    data = response.json()
+    return data
 
 
 if __name__ == "__main__":
